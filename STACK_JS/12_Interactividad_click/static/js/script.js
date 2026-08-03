0a1ae8f7a7ed07d479303bf43c05bf0785410232
Gabrilinsky <gabrielnaranjo@liceovvh.cl>
2026-08-03 16:04:22 -0400

    tarea

--- STACK_JS/12_Interactividad_click/static/js/script.js
+++ STACK_JS/12_Interactividad_click/static/js/script.js
@@ -1,31 +1,33 @@
 document.getElementById("colorButton").addEventListener("click", function () {
-   cambiarColor();
+   cambiarColorbody();
 });
 
-// Función que cambia el color de fondo al hacer clic
-function cambiarColor() {
-   let colores = ["#ff6347", "#66cdaa", "#9370db", "#ffd700", "#4682b4", "#ffa07a"];
-   let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-   document.body.style.backgroundColor = colorAleatorio;
-}
 
+document.getElementById("button").addEventListener("click", function () {
+   cambiarColorcaja2();
+});
 document.getElementById("color").addEventListener("click", function () {
-   cambiarColor();
+   cambiarColorcaja1();
 });
 
-function cambiarColor() {
-   let colores = ["#00f74a", "#3ff0b5", "#0339e9", "#f1134a", "#ff09c1", "#a60dff"];
+function cambiarColorbody() {
+   let colores = ["#ff3232", "#03f123", "#d3f800", "#00aaf8a8", "#ff0062", "#f84802"];
    let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-   document.body.style.backgroundColor = colorAleatorio;
+   const elemento = document.querySelector("body");
+   elemento.style.backgroundColor = colorAleatorio
 }
 
 
-document.getElementById("Button").addEventListener("click", function () {
-   cambiarColor();
-});
-
-function cambiarColor() {
-   let colores = ["#f30000", "#03f123", "#d3f800", "#00aaf8a8", "#ff0062", "#f84802"];
+function cambiarColorcaja1() {
+   let colores = ["#ff3232", "#03f123", "#d3f800", "#00aaf8a8", "#ff0062", "#f84802"];
    let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-   document.body.style.backgroundColor = colorAleatorio;
+   const elemento = document.querySelector("#caja1");
+   elemento.style.backgroundColor = colorAleatorio
+
+}
+function cambiarColorcaja2() {
+   let colores = ["#ff3232", "#03f123", "#d3f800", "#00aaf8a8", "#ff0062", "#f84802"];
+   let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+   const elemento = document.querySelector("#caja2");
+   elemento.style.backgroundColor = colorAleatorio
 }
