75537ad4e864615198de1ba5df92254995d4264a
Gabrilinsky <gabrielnaranjo@liceovvh.cl>
2026-08-04 14:44:11 -0400

    Update script.js

--- STACK_JS/13_Concepto_this_(Esto)/static/js/script.js
+++ STACK_JS/13_Concepto_this_(Esto)/static/js/script.js
@@ -1,19 +1,32 @@
 //Código en JavaScript
 
 // Seleccionamos el botón por su id
-   let boton = document.getElementById("cambiarTexto");
-boton.addEventListener("click", function () {
-this.innerText = "Texto Cambiado"
-});
+let boton = document.getElementById("cambiarTexto");
 
+boton.addEventListener("click", function () {
+    if (this.innerText === "Haz clic en mí") {
+        this.innerText = "¡Texto cambiado!";
+    } else {
+        this.innerText = "Haz clic en mí";
+    }
+});
 
  let boton2 = document.getElementById("cambiarTexto2");
+
 boton2.addEventListener("click", function () {
-this.innerText = " Segundo Texto Cambiado"
+    if (this.innerText === "Haz clic en mí") {
+        this.innerText = "¡Texto cambiado!";
+    } else {
+        this.innerText = "Haz clic en mí";
+    }
 });
 
-
 let boton3 = document.getElementById("cambiarTexto3");
+
 boton3.addEventListener("click", function () {
-this.innerText = " Tercer Texto Cambiado"
+    if (this.innerText === "Haz clic en mí") {
+        this.innerText = "¡Texto cambiado!";
+    } else {
+        this.innerText = "Haz clic en mí";
+    }
 });
