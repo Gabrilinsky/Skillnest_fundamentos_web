08018e998253967a1578352a4b5db90004aa8d1c
Gabrilinsky <gabrielnaranjo@liceovvh.cl>
2026-08-04 14:40:10 -0400

    tarea

--- STACK_JS/13_Concepto_this_(Esto)/static/js/script.js
+++ STACK_JS/13_Concepto_this_(Esto)/static/js/script.js
@@ -1,10 +1,19 @@
 //Código en JavaScript
 
 // Seleccionamos el botón por su id
-let boton = document.getElementById("cambiarTexto");
-
-// Agregamos un evento 'click' al botón
+   let boton = document.getElementById("cambiarTexto");
 boton.addEventListener("click", function () {
-   // Cambia el texto del botón usando `this`
-   this.innerText = "¡Texto cambiado!";
+this.innerText = "Texto Cambiado"
+});
+
+
+ let boton2 = document.getElementById("cambiarTexto2");
+boton2.addEventListener("click", function () {
+this.innerText = " Segundo Texto Cambiado"
+});
+
+
+let boton3 = document.getElementById("cambiarTexto3");
+boton3.addEventListener("click", function () {
+this.innerText = " Tercer Texto Cambiado"
 });
